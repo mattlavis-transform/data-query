cac619ea3b30c38cac096b531ba64903d54df43c
Matt Lavis <matthew.lavis@gmail.com>
2023-01-04 10:23:21 +0000

    Grey scope

--- grep.py
+++ grep.py
@@ -4,27 +4,34 @@ import sys
 import string
 from codecs import decode
 from dotenv import load_dotenv
+import classes.functions as f
 
+
+f.clear()
 
 load_dotenv('.env')
 
 if len(sys.argv) > 1:
     scope = sys.argv[1].lower()
+    if scope in ("eu", "xi"):
+        scope = "tgb"
+
     scopes = {
         "cds": 'CDS_DATA_FOLDER',
         "tgb": 'TGB_DATA_FOLDER',
         "dit": 'DIT_DATA_FOLDER',
     }
+    if scope not in scopes:
+        scope = "cds"
+
     if scope in scopes:
-        scope = scopes[scope]
+        scope2 = scopes[scope]
     else:
-        scope = scopes["cds"]
+        scope2 = scopes["cds"]
 
     # Get the argument
     if len(sys.argv) > 2:
-        # xml_path = os.getenv('DIT_DATA_FOLDER')
-        # xml_path = os.getenv('CDS_DATA_FOLDER')
-        xml_path = os.getenv(scope)
+        xml_path = os.getenv(scope2)
 
         if len(sys.argv) > 3:
             try:
@@ -42,22 +49,17 @@ if len(sys.argv) > 1:
             term = "<" + term + ">"
 
         grepped_filename = term.translate(str.maketrans('', '', string.punctuation)) + ".txt"
-        grep_path = os.path.join(os.getcwd(), "resources", "grep")
+        grep_path = os.path.join(os.getcwd(), "resources", "grep", scope)
         grep_path2 = os.path.join(grep_path, grepped_filename)
 
         print("\nSearching for term '{term}' in folder '{grep_path2}'.\n".format(term=term, grep_path2=grep_path2))
 
-        # grep_string = "grep -i -r --include='*.xml' '{term}' '{xml_path}' > '{grep_path2}'".format(
-        #     term=term,
-        #     xml_path=xml_path,
-        #     grep_path2=grep_path2
-        # )
         grep_string = "grep -r --include='*.xml' '{term}' '{xml_path}' > '{grep_path2}'".format(
             term=term,
             xml_path=xml_path,
             grep_path2=grep_path2
         )
-        print("\n" + grep_string + "\n")
+        print("\nRunning grep command:\n\n" + grep_string + "\n")
         ret = os.system(grep_string)
 
         # Sort the file by date
